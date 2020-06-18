6dfcc1fe8d0b34997c0e45c5527e80e1afa1eac8
alu0100813272 <alu0100813272@ull.edu.es>
2020-06-18 21:18:11 +0100

    nuevo

---
+++ src/main/java/uiManager/UIManagerTUT.java
@@ -0,0 +1,12 @@
+package uiManager;
+
+public class UIManagerTUT extends JFrame{
+	public UIManagerTUT() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
